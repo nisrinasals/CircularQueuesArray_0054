e5fac95874eaa7100e46dbe2e47ec19fe1c055f6
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-31 15:25:30 +0700

    void insert selesai

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -24,5 +24,20 @@ public:
             cout << "\nQueue overflow\n";
             return;
         } 
+    
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            //Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if(REAR == max - 1) 
+                REAR = 0;
+            else 
+                REAR = REAR +1;
+        }
+        queue_array[REAR] = num;
     }
+
 };
